4b529929bc990dcf36c06f463dbea78ccda17b21
Daniel Fiala <freelancer@meave.biz>
2020-05-28 15:26:04 +0200

    t3z0s: Parse of first packet + export C headers + logger for debugging.

--- tezos/dissector/t3z0s/t3z0s.c
+++ tezos/dissector/t3z0s/t3z0s.c
@@ -3,14 +3,20 @@
 #include "config.h"
 #include <epan/packet.h>
 #include <epan/conversation.h>
+#include <epan/dissectors/packet-tcp.h>
+#include <stdio.h>
 
-/* This is defined in Rust */
+/* This is shared with Rust */
+
 struct T3zosDissectorInfo {
+    int hf_payload_len;
+    int hf_packet_counter;
     int hf_phrase;
     int hf_word;
 };
 
-extern int t3z03s_dissect_packet(struct T3zosDissectorInfo*, tvbuff_t*, proto_tree*);
+extern int t3z03s_dissect_packet(struct T3zosDissectorInfo*, tvbuff_t*, proto_tree*, const struct tcp_analysis*);
+extern int t3z03s_free_conv_data(void*);
 /* End of section shared with Rust */
 
 static dissector_handle_t t3z0s_handle;
@@ -18,8 +24,28 @@ static int proto_t3z0s = -1;
 static struct T3zosDissectorInfo info = {
     -1,
     -1,
+    -1,
+    -1,
 };
 static gint ett_t3z0s = -1; // Subtree
+
+static gboolean wmem_cb(wmem_allocator_t* allocator, wmem_cb_event_t ev, void *data)
+{
+    switch (ev) {
+        case WMEM_CB_FREE_EVENT:
+            {FILE *f = fopen("/tmp/xyz.log", "a");
+            fprintf(f, "Freeing memory allocator: %p %p\n", allocator, data);
+            fclose(f);}
+            t3z03s_free_conv_data(data);
+            break;
+        case WMEM_CB_DESTROY_EVENT:
+            {FILE *f = fopen("/tmp/xyz.log", "a");
+            fprintf(f, "destroy: %p\n", allocator);
+            fclose(f);}
+    }
+
+    return TRUE;
+}
 
 /** An old style dissector proxy.
  *
@@ -28,11 +54,26 @@ static gint ett_t3z0s = -1; // Subtree
 static
 int dissect_t3z0s_old(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void* data)
 {
-    (void)pinfo;
     (void)data;
+    conversation_t *conv = find_or_create_conversation(pinfo);
+    DISSECTOR_ASSERT_HINT(conv, "find_or_create_conversation() returned NULL");
+
+    // FIXME: Is callback registered only once?
+    struct tcp_analysis *tcpd = get_tcp_conversation_data(conv, pinfo);
+    void *convd = conversation_get_proto_data(conv, proto_t3z0s);
+    if (!convd)
+    {
+        conversation_add_proto_data(conv, proto_t3z0s, (void*)0x1);
+        wmem_register_callback(wmem_file_scope(), wmem_cb, tcpd);
+    }
+
     proto_item *ti = proto_tree_add_item(tree, proto_t3z0s, tvb, 0, -1, ENC_NA);
     proto_tree *t_tree = proto_item_add_subtree(ti, ett_t3z0s);
-    return t3z03s_dissect_packet(&info, tvb, t_tree);
+    proto_tree_add_int64_format(t_tree, info.hf_payload_len, tvb, 0, 0, (int64_t)conv, "T3z0s conversation: %p", conv); // XYZ: Dbg.
+    {FILE *f = fopen("/tmp/xyz.log", "a");
+    fprintf(f, "conv: %p %p\n", wmem_file_scope(), conv);
+    fclose(f);}
+    return t3z03s_dissect_packet(&info, tvb, t_tree, tcpd);
 }
 
 static gboolean
@@ -54,6 +95,16 @@ void
 proto_register_t3z0s(void)
 {
     static hf_register_info hf[] = {
+        { &info.hf_packet_counter,
+            { "T3z0s Packet Counter", "t3z0s.packet_counter",
+            FT_INT64, BASE_DEC,
+            NULL, 0x0, NULL, HFILL }
+        },
+        { &info.hf_payload_len,
+            { "T3z0s Payload Length", "t3z0s.payload_len",
+            FT_INT64, BASE_DEC,
+            NULL, 0x0, NULL, HFILL }
+        },
         { &info.hf_phrase,
             { "T3z0s Phrase", "t3z0s.phrase",
             FT_STRING, BASE_NONE,
--- tezos/dissector/t3z0s/t3z0s_rs/src/lib.rs
+++ tezos/dissector/t3z0s/t3z0s_rs/src/lib.rs
@@ -1,9 +1,38 @@
 extern crate libc;
-use libc::{c_int, c_uint};
+use libc::{c_int, c_uint, c_void};
+use std::{
+    boxed::Box,
+    collections::HashMap,
+    option::Option,
+    net::SocketAddr,
+    convert::TryFrom,
+};
 
+use failure::Error;
+
+use crypto::{
+    hash::HashType,
+    crypto_box::precompute,
+    nonce::{NoncePair, generate_nonces},
+};
+use tezos_messages::p2p::{
+    binary_message::BinaryChunk,
+};
+
+// TODO: DRF: Move ConnectionMessage from tezedge-debugger to some library or turn tezedge-debugger to a mod?
+mod connection_message;
+use connection_message::ConnectionMessage;
+
+mod logger;
+use logger::msg;
+
+mod wireshark;
+use wireshark::packet::packet_info;
 
 // Opaque structs from Wireshark
 #[repr(C)] pub struct tvbuff_t { _private: [u8; 0] }
+#[repr(C)] pub struct tcp_analysis { _private: [u8; 0] }
+#[repr(C)] pub struct proto_item { _private: [u8; 0] }
 #[repr(C)] pub struct proto_tree { _private: [u8; 0] }
 #[repr(C)] pub struct wmem_allocator_t { _private: [u8; 0] }
 
@@ -13,6 +42,14 @@ extern "C" {
     fn tvb_get_ptr(tvb: *mut tvbuff_t, offset: c_int /* gint */, length: c_int /* gint */) -> *mut u8;
     fn tvb_captured_length(tvb: *mut tvbuff_t) -> c_uint /* guint */;
     fn wmem_packet_scope() -> *mut wmem_allocator_t;
+    fn proto_tree_add_int64(
+        proto_tree: *mut proto_tree,
+        hfindex : c_int,
+        tvb: *mut tvbuff_t,
+        start: c_int,
+        length: c_int,
+        value: i64
+    ) -> *mut proto_item;
     fn proto_tree_add_item_ret_string_and_length(
         proto_tree: *mut proto_tree,
         hfindex : c_int,
@@ -29,8 +66,25 @@ extern "C" {
 // Struct that represents static data on C side
 #[repr(C)]
 pub struct T3zosDissectorInfo {
+    hf_payload_len: c_int,
+    hf_packet_counter: c_int,
     hf_phrase: c_int,
     hf_word: c_int
+}
+
+struct Conversation {
+    counter: u32,
+}
+impl Conversation {
+    pub fn new() -> Self {
+        Conversation { counter: 0 }
+    }
+
+    pub fn process_unencrypted_msg(self: &Self, payload: Vec<u8>) -> Result<ConnectionMessage, Error> {
+        let chunk = BinaryChunk::try_from(payload)?;
+        let conn_msg = ConnectionMessage::try_from(chunk)?;
+        Ok(conn_msg)
+    }
 }
 
 fn get_info_safe<'a>(p_info: *const T3zosDissectorInfo) -> &'a T3zosDissectorInfo {
@@ -55,6 +109,17 @@ fn tvb_get_guint8_safe(tvb: *mut tvbuff_t, offset: c_int /* gint */) -> u8 {
 
 fn tvb_captured_length_safe(tvb: *mut tvbuff_t) -> c_uint {
     unsafe { tvb_captured_length(tvb) }
+}
+
+fn proto_tree_add_int64_safe(
+    proto_tree: *mut proto_tree,
+    hfindex : c_int,
+    tvb: *mut tvbuff_t,
+    start: c_int,
+    length: c_int,
+    value: i64
+) -> *mut proto_item {
+    unsafe { proto_tree_add_int64(proto_tree, hfindex, tvb, start, length, value) }
 }
 
 fn proto_tree_add_item_safe(
@@ -82,7 +147,6 @@ fn proto_tree_add_item_safe(
         );
     }
 }
-
 
 fn add_word(info: &T3zosDissectorInfo, tvb: *mut tvbuff_t, proto_tree: *mut proto_tree, wbeg: c_int, wend: c_int) {
     let wlen = wend - wbeg;
@@ -115,15 +179,41 @@ fn add_words(info: &T3zosDissectorInfo, tvb: *mut tvbuff_t, proto_tree: *mut pro
     add_word(info, tvb, proto_tree, prev_space + 1, len);
 }
 
+static mut conversations_map: Option<HashMap<*const tcp_analysis, Conversation>> = None;
+
+fn get_conv_map() -> &'static mut HashMap<*const tcp_analysis, Conversation> {
+    unsafe { conversations_map.get_or_insert(HashMap::new()) }
+}
+
+#[no_mangle]
+pub extern "C" fn t3z03s_free_conv_data(p_data: *mut c_void) {
+    get_conv_map().remove(&(p_data as *const tcp_analysis));
+}
+
 #[no_mangle]
 pub extern "C" fn t3z03s_dissect_packet(
         p_info: *const T3zosDissectorInfo,
-        tvb: *mut tvbuff_t, proto_tree: *mut proto_tree
+        tvb: *mut tvbuff_t, proto_tree: *mut proto_tree,
+        tcpd: *const tcp_analysis
 ) -> c_int {
     let info = get_info_safe(p_info);
 
     let ulen = tvb_captured_length_safe(tvb);
     let len = ulen as c_int;
+
+    let opt_conv = get_conv_map().get_mut(&tcpd);
+    let conv = match opt_conv {
+        Some(x) => x,
+        None => {
+            get_conv_map().insert(tcpd, Conversation::new());
+            get_conv_map().get_mut(&tcpd).unwrap()
+        }
+    };
+
+    conv.counter += 1;
+
+    proto_tree_add_int64_safe(proto_tree, info.hf_payload_len, tvb, 0, 0, len as i64);
+    proto_tree_add_int64_safe(proto_tree, info.hf_packet_counter, tvb, 0, 0, conv.counter as i64);
 
     proto_tree_add_item_safe(
         proto_tree,
@@ -135,6 +225,11 @@ pub extern "C" fn t3z03s_dissect_packet(
     );
 
     add_words(info, tvb, proto_tree, len);
+
+    let payload = get_data_safe(tvb);
+    let conn_msg_opt = conv.process_unencrypted_msg(payload.to_vec());
+
+    msg(format!("packet: count:{} conn_msg_opt: {:?}", conv.counter, conn_msg_opt));
 
     len
 }
