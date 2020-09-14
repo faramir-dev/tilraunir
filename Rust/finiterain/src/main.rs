51485f99ab26bbb64cd71dd07e0eeab98cf98086
Daniel R. Fiala <freelancer@meave.biz>
2020-09-14 11:11:21 +0200

    Add formatter for slices.

--- Rust/finiterain/src/main.rs
+++ Rust/finiterain/src/main.rs
@@ -5,10 +5,24 @@ mod slow;
 
 use anyhow;
 use num_rational::Rational64;
+use std::fmt;
 use std::vec::Vec;
 
 const MAX: Rational64 = Rational64::new_raw(std::u32::MAX as i64 * 2, 1);
 const ZERO: Rational64 = Rational64::new_raw(0, 1);
+
+struct Fmt<'a, T: fmt::Display>(&'a [T]);
+impl<T: fmt::Display> fmt::Display for Fmt<'_, T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for (i, v) in self.0.iter().enumerate() {
+            if i != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}", v)?;
+        }
+        Ok(())
+    }
+}
 
 fn main() {
     let maybe_err = || -> anyhow::Result<()> {
@@ -16,11 +30,7 @@ fn main() {
         println!(
             "> total_time: {}; landscape: {};",
             cfg.total_time,
-            cfg.landscape[1..]
-                .iter()
-                .fold(cfg.landscape[0].to_string(), |acc, &num| acc
-                    + ", "
-                    + &num.to_string())
+            Fmt(&cfg.landscape[1..])
         );
 
         let mut landscape = vec![MAX];
@@ -36,14 +46,7 @@ fn main() {
 
         let time = Rational64::from_integer(cfg.total_time);
         slow::calculate(time, &mut landscape);
-        println!(
-            "> Result: : {};",
-            landscape[2..landscape.len() - 2]
-                .iter()
-                .fold(landscape[1].to_string(), |acc, &num| acc
-                    + ", "
-                    + &num.to_string())
-        );
+        println!("> Result: : {};", Fmt(&landscape[1..landscape.len() - 2]));
 
         Ok(())
     }();
